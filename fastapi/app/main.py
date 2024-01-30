e62e5dcdf4b334735040429cf73afb9a76458a78
olin_tp_ar <universalexperimental@protonmail.com>
2024-01-30 18:47:53 +0100

    FastAPI: Query with date ranges and turbine id working with ISO date format

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from datetime import datetime
 from pymongo import MongoClient
 
 app = FastAPI()
@@ -10,10 +11,10 @@ mongo_db = 'turbit'
 client = MongoClient(mongo_host, mongo_port)
 db = client[mongo_db]
 
-def retrieve_data(turbineId: int):
+def retrieve_data(query):
     turbines = db['turbines_data']
     data = []
-    for item_data in turbines.find({"turbineId": turbineId}):
+    for item_data in turbines.find(query):
         del item_data['_id']
         data.append(item_data)
     return data
@@ -23,5 +24,10 @@ def index():
     return {"Task": "Time Series Data Handling and API Development"}
 
 @app.get("/turbines/{turbineId}")
-def turbines(turbineId: int):
-    return retrieve_data(turbineId)
+def turbines(
+        turbineId: int = Path(..., title="Turbine ID"),
+        start_date: datetime = Query(..., title="Start date and time in ISO format"),
+        end_date: datetime = Query(..., title="End date and time in ISO format")
+        ):
+    query = {"turbineId": turbineId, "Dat/Zeit": {"$gte": start_date, "$lte": end_date}}
+    return retrieve_data(query)
