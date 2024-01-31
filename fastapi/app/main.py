61f53e25a53f7fa3928dc367b7638a64ce4f047b
olin_tp_ar <universalexperimental@protonmail.com>
2024-01-31 14:51:40 +0100

    FastAPI: Optional query parameters, turbine id and start and end date. Results sorted by date

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -14,7 +14,7 @@ db = client[mongo_db]
 def retrieve_data(query):
     turbines = db['turbines_data']
     data = []
-    for item_data in turbines.find(query):
+    for item_data in turbines.find(query).sort("Dat/Zeit"):
         del item_data['_id']
         data.append(item_data)
     return data
@@ -23,11 +23,22 @@ def retrieve_data(query):
 def index():
     return {"Task": "Time Series Data Handling and API Development"}
 
-@app.get("/turbines/{turbineId}")
+@app.get("/turbine-stats")
 def turbines(
-        turbineId: int = Path(..., title="Turbine ID"),
-        start_date: datetime = Query(..., title="Start date and time in ISO format"),
-        end_date: datetime = Query(..., title="End date and time in ISO format")
+        turbine_id: int = Query(None, title="The turbine id"),
+        start_date: datetime = Query(None, title="Start date and time in ISO format"),
+        end_date: datetime = Query(None, title="End date and time in ISO format")
         ):
-    query = {"turbineId": turbineId, "Dat/Zeit": {"$gte": start_date, "$lte": end_date}}
+    
+    query = {}
+    if turbine_id is not None:
+        query["turbineId"] = turbine_id
+    if start_date:
+        query["Dat/Zeit"] = {"$gte": start_date}
+    if end_date:
+        if query["Dat/Zeit"]:
+            query["Dat/Zeit"]["$lte"] = end_date
+        else:
+            query["Dat/Zeit"] = {"$lte": end_date}
+
     return retrieve_data(query)
